63b3349965d154f80113d5a833eee900740bb7e0
rafael1199v <123646379+rafael1199v@users.noreply.github.com>
2025-05-07 08:14:00 -0400

    Mejoras SideBar

--- GestionAcademica.Client/src/components/SideBar.jsx
+++ GestionAcademica.Client/src/components/SideBar.jsx
@@ -1,30 +1,31 @@
 import React, { useState } from "react";
 import SideBarItem from "./SideBarItem";
 import { SIDE_BAR_ITEM } from "../config/SideBarItem-const";
+import { HomeIcon, ArrowLeftIcon } from "@heroicons/react/16/solid";
 
 export function SideBar() {
-
   const [selectedItem, setSelectedItem] = useState(SIDE_BAR_ITEM.PROFESSOR);
 
   return (
-    <div className="flex h-screen flex-col justify-between border-e border-gray-100 bg-white">
+    <div className="w-64 h-screen flex-col justify-between border-e border-gray-100 bg-white">
       <div className="px-4 py-6">
         <span className="grid h-10 w-32 place-content-center rounded-lg bg-gray-100 text-xs text-gray-600">
           Gestion Academica
         </span>
 
         <ul className="mt-6 space-y-1">
-          <SideBarItem 
+          <SideBarItem
             text="General"
             isSelected={selectedItem === SIDE_BAR_ITEM.PROFESSOR}
             onClick={() => setSelectedItem(SIDE_BAR_ITEM.PROFESSOR)}
+            icon={<HomeIcon className="w-4 h-4" />}
           />
-          <SideBarItem 
+          <SideBarItem
             text="Cerrar sesion"
             isSelected={selectedItem === SIDE_BAR_ITEM.SIGN_OUT}
             onClick={() => setSelectedItem(SIDE_BAR_ITEM.SIGN_OUT)}
+            icon={<ArrowLeftIcon className="w-4 h-4" />}
           />
-
         </ul>
       </div>
     </div>
--- GestionAcademica.Client/src/components/SideBarItem.jsx
+++ GestionAcademica.Client/src/components/SideBarItem.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-function SideBarItem({isSelected, text, onClick}) {
+function SideBarItem({isSelected, text, onClick, icon}) {
   return (
     <li onClick={onClick}>
+        
         <a
         href="#"
-        className={`block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700 ${isSelected ? 'bg-gray-100 text-gray-700' : ''}`}
+        className={`flex flex-row items-center gap-3 rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700 ${isSelected ? 'bg-gray-100 text-gray-700' : ''}`}
         >
+        {icon}
         {text}
         </a>
   </li>
