1082df2541354e42a3f1a9751caa37806806501a
rafael1199v <123646379+rafael1199v@users.noreply.github.com>
2025-05-15 09:16:52 -0400

    Arreglos en auth0

--- GestionAcademica.API/HasScopeRequirement.cs
+++ GestionAcademica.API/HasScopeRequirement.cs
@@ -7,9 +7,10 @@ public class HasScopeRequirement : IAuthorizationRequirement
     public string Issuer { get; set; }
     public string Scope { get; set; }
 
-    public HasScopeRequirement(string issuer, string scope)
+    public HasScopeRequirement(string scope, string issuer)
     {
         Scope = scope ?? throw new ArgumentNullException(nameof(scope));
         Issuer = issuer ?? throw new ArgumentNullException(nameof(issuer));
     }
+    
 }
