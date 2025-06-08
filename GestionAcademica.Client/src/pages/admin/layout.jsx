76f0c35ba04ec7db3750aae66b2d363dab29aa78
DanyRoland <64906201+DanyElAlgo@users.noreply.github.com>
2025-06-08 17:08:59 -0400

    aaaaaaaaaaa
    help

--- GestionAcademica.Client/src/pages/admin/layout.jsx
+++ GestionAcademica.Client/src/pages/admin/layout.jsx
@@ -1,5 +1,7 @@
-import { Outlet } from "react-router-dom";
-import SideBar from "../../components/side-bar";
+import React, { useEffect } from "react";
+import SideBar from "../components/side-bar";
+import { useAuth } from "./AuthContext";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function Layout({ children }) {
   const { isAuthenticated } = useAuth();
@@ -21,9 +23,7 @@ function Layout({ children }) {
   return (
     <div className="flex min-h-screen">
       <SideBar />
-      <div className="flex-1 p-8">
-        <Outlet />
-      </div>
+      <div className="flex-1 p-8">{children}</div>
     </div>
   );
 }
