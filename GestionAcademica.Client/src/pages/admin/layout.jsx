2fe2299b95e1653e65ce773623ebb06744fcb150
rafael1199v <123646379+rafael1199v@users.noreply.github.com>
2025-06-08 12:36:51 -0400

    Cambio de layout dependiendo del tipo de rol

---
+++ GestionAcademica.Client/src/pages/admin/layout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import SideBar from "../../components/side-bar";
+
+function Layout() {
+  return (
+    <div className="flex min-h-screen">
+      <SideBar />
+      <div className="flex-1 p-8">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
