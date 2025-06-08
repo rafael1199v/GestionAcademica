8541b743d216c5b9380be8ad9de0734890933201
rafael1199v <123646379+rafael1199v@users.noreply.github.com>
2025-06-08 11:23:50 -0400

    Autenticación mejorada
    Se arregló el problema de la recarga de autenticación y se mejoró la estructura de las rutas protegidas para dinamizar la estructura de la aplicación dependiendo del rol

--- GestionAcademica.Client/src/main.jsx
+++ GestionAcademica.Client/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { AuthContextProvider } from "./context/AuthContext";
 import "./index.css";
-import App from "./App.jsx";
+import App from './app'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </StrictMode>
 );
--- GestionAcademica.Client/src/pages/layout.jsx
+++ GestionAcademica.Client/src/pages/layout.jsx
@@ -1,30 +1,13 @@
-import React, { useEffect } from "react";
+import { Outlet } from "react-router-dom";
 import SideBar from "../components/side-bar";
-import { useAuth } from "./AuthContext";
-import { useNavigate, useLocation } from "react-router-dom";
 
 function Layout({ children }) {
-  const { isAuthenticated } = useAuth();
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  useEffect(() => { 
-    if (!isAuthenticated) {
-      navigate("/login");
-      console.log("ASDASDASD"); 
-    };
-  }, []);
-
-  const isLoginPage = location.pathname === "/login";
-
-  if (isLoginPage) {
-    return <div className="flex-1">{children}</div>;
-  }
-  
   return (
     <div className="flex min-h-screen">
       <SideBar />
-      <div className="flex-1 p-8">{children}</div>
+      <div className="flex-1 p-8">
+        <Outlet />
+      </div>
     </div>
   );
 }
